ea750a18f0a799c0eeb88b48ef7a4e95083c3b24
Julio Cesar <js.cesar42@gmail.com>
2021-03-19 23:14:57 -0300

    user creation form implemented

--- user_authentication/forms.py
+++ user_authentication/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class registrationForm(forms.Form):
+    name = forms.CharField(label='Type your name here. Ex: FirstName LastName', max_length=100)
     email = forms.EmailField(label='Type your email adress here', max_length= 100)
     password = forms.CharField(widget=forms.PasswordInput)
 
--- user_authentication/views.py
+++ user_authentication/views.py
@@ -2,27 +2,36 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .forms import *
 
-# Create your views here.
-
 def testView(request):
     return render(request, 'test.html')
 
 def createTest(request):
 
-    #user =  User.objects.create_user('leko', 'leko@gmail.com', 'teste@123')
-    #user.last_name = 'Santos'
-    #user.save()
-
     if request.method == 'POST':
         form = registrationForm(request.POST)
-        
-        email = 'test'
         if form.is_valid():
-            return render(request, 'create.html', {'email':email})
+            #Get values form
+            form_values = request.POST.dict()
+            username = form_values.get('name')
+            email = form_values.get('email')
+            password = form_values.get('password')
+            
+            #Checks if email is already registered 
+            all_users = User.objects.all()
+            has_email_in_database = False
+            for one_user in all_users:
+                email_user = one_user.email
+                if email_user == email:
+                    has_email_in_database = True
+                    break
+            #Create user if it does not exist
+            if has_email_in_database == False:
+                user =  User.objects.create_user(f'{username}',f'{email} ',f'{password}')
+                user.save()
+                return render(request, 'create.html', {'teste':has_email_in_database})
+            else:
+                form = registrationForm()
+                return render(request, 'has_email_in_database.html')
     else:
         form = registrationForm()
         return render(request, 'create_user.html', {'form':form})
-        
-
-
-    #return render(request, 'create.html', {'name':name})
