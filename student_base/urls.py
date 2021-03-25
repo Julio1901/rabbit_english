4a327cffeaf419db5acbe8635d589df34a91df61
Julio Cesar <juliocesar@localhost.localdomain>
2021-03-25 09:36:48 -0300

    view to insert word in db finshed and view to display words by user started

--- student_base/urls.py
+++ student_base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('add-word', views.insertWord, name='insert-word')
+    path('add-word', views.insertWord, name='insert-word'),
+    path('display-all-words', views.DisplayAllWords, name='display-all-words'),
 ]
